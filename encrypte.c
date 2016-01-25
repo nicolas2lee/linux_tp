26672325186441ab7b58ed622245dbb887202e22
nicolas2lee <nicolas2lee@gmail.com>
2016-01-25 18:04:01 +0100

    add encryote.c version 1.1

--- encrypte.c
+++ encrypte.c
@@ -15,50 +15,66 @@ void encryptDecrypt(char *input, char *output) {
 
 int main(){
   int input_fd;
-  int output_fd;
-	int recover_fd;
+  int input2_fd;
+  int encrypt_fd;
+  int decrypt_fd ;
   ssize_t ret_in, ret_out, ret_recover;
   char buffer[BUF_SIZE];
+  char buffer2[BUF_SIZE];
 	char out_buffer[BUF_SIZE];
 	char origin_buffer[BUF_SIZE];
 //here is the path of the file in your usb
+//   /media/u/KINGSTON
   input_fd=open("/mnt/toCrypt.txt",O_RDONLY);
+  //input_fd=open("/media/u/KINGSTON/toCrypt.txt",O_RDONLY);	
   if (input_fd==-1){
     perror("open");
-    return -1;
-  }
-  /* Create output file descriptor */
-  output_fd = open("/mnt/output.txt", O_WRONLY | O_CREAT, 0644);
-  if(output_fd == -1){
-      perror("open");
-      return -2;
   }
 
-  recover_fd = open("recover.txt", O_WRONLY | O_CREAT, 0644);
-  if(recover_fd == -1){
+ input2_fd=open("/mnt/toDecrypt.txt",O_RDONLY);
+  if (input2_fd==-1){
+    perror("open");
+  }
+
+  /* Create output file descriptor */
+  encrypt_fd = open("/mnt/crypted.txt", O_WRONLY | O_CREAT, 0644);
+  if(encrypt_fd == -1){
       perror("open");
-      return -3;
+  
+  }
+
+  decrypt_fd = open("/mnt/decrypted.txt", O_WRONLY | O_CREAT, 0644);
+  if(decrypt_fd == -1){
+      perror("open");
+      
   }
  
   /* Copy process */
   while((ret_in = read (input_fd, &buffer, BUF_SIZE)) > 0){
 		encryptDecrypt(&buffer,&out_buffer);
-    ret_out = write (output_fd, &out_buffer, (ssize_t) ret_in);
+    ret_out = write (encrypt_fd, &out_buffer, (ssize_t) ret_in);
     if(ret_out != ret_in){
 			perror("write");
       return -3;
     }
-		encryptDecrypt(&out_buffer,&origin_buffer);
-    ret_recover = write (recover_fd, &origin_buffer, (ssize_t) ret_out);
-    if(ret_out != ret_recover){
+
+  }
+
+  while((ret_in = read (input2_fd, &buffer2, BUF_SIZE)) > 0){
+		encryptDecrypt(&buffer2,&out_buffer);
+    ret_out = write (decrypt_fd, &out_buffer, (ssize_t) ret_in);
+    if(ret_out != ret_in){
 			perror("write");
       return -3;
     }
+
   }
+
  
     /* Close file descriptors */
   close (input_fd);
-  close (output_fd);
-	close (recover_fd);
+  close (encrypt_fd);
+	close (input2_fd);
+	close (decrypt_fd);
   return 0;
 }
